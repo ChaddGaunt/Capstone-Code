e2983f03e83466f1ddb27ee0c2be2b9af4468483
Chadd Gaunt <Chadd.Gaunt@student.uts.edu.au>
2024-04-04 19:27:23 +1100

    updated data file name

--- PF/load_and_discard_bad_data.m
+++ PF/load_and_discard_bad_data.m
@@ -1,8 +1,8 @@
 close all;
 
-data_table = readtable('synced_data_output.xlsx');
+data_table = readtable('Interpolated_data.xlsx');
 
-T = data_table.UWB_timestamp';
+T = data_table.timestamp';
 X = data_table.UWB_x';
 Y = data_table.UWB_y';
 
