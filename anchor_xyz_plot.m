a3b6e112d1b0824d2c80c4911d60724a20cda6f6
Chadd Gaunt <Chadd.Gaunt@student.uts.edu.au>
2024-04-04 19:35:16 +1100

    upload working code

--- anchor_xyz_plot.m
+++ anchor_xyz_plot.m
@@ -2,19 +2,17 @@ PF_table = readtable('Interpolated_data.xlsx');
 anchorlocations = readtable('UWB Device Locations AdNav.csv');
 table = readtable("PF_var_2.5_Associate_10.csv");
 
-% Plot the first line
-plot(table.x, table.y, '-o', 'MarkerSize', 2.5); % Plot with circles as markers
-hold on; % Hold the current plot so that subsequent plots are added to it
 
-% Plot the second line
-% Assuming you have x and y data in 'table'
+plot(table.x, table.y, '-o', 'MarkerSize', 2.5); 
+hold on; 
+
 plot(PF_table.LiDAR_x, PF_table.LiDAR_y, 'o-', 'MarkerSize', 2.5); % Plot with circles as markers
 
 plot(anchorlocations.x, anchorlocations.y, 'o', 'MarkerSize', 5);
 
 R = eul2rotm([anchorlocations.psi, anchorlocations.theta, anchorlocations.phi], 'ZYX');
 
-% Adding arrows for direction
+% Adding arrows for anchor direction
 for i = 1:length(anchorlocations.x)
     % Extract the rotation matrix for the current point
     R_current = R(:,:,i);
@@ -39,7 +37,6 @@ ylabel('Y');
 title('Align PF UWB and LiDAR Trajectories');
 grid on;
 
-% Legend to differentiate between the lines
 legend('UWB Trajectory (PF Results)', 'LiDar Trajectory (Ground Truth)');
 
 
