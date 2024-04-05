0a037fec207b69bcc927cca2f004397907045c54
Chadd Gaunt <Chadd.Gaunt@student.uts.edu.au>
2024-04-05 13:06:10 +1100

    Updated code to plot colour coded x,y,z locations according to UWB anchor number

--- anchor_xyz_plot.m
+++ anchor_xyz_plot.m
@@ -1,14 +1,43 @@
-PF_table = readtable('Interpolated_data.xlsx');
-anchorlocations = readtable('UWB Device Locations AdNav.csv');
-table = readtable("PF_var_2.5_Associate_10.csv");
-
-
-plot(table.x, table.y, '-o', 'MarkerSize', 2.5); 
 hold on; 
+raw_data = readtable('Interpolated_data.xlsx');
+anchorlocations = readtable('UWB Device Locations AdNav.csv');
+PF_Data = readtable("PF_var_2.5_Associate_10.csv");
 
-plot(PF_table.LiDAR_x, PF_table.LiDAR_y, 'o-', 'MarkerSize', 2.5); % Plot with circles as markers
 
-plot(anchorlocations.x, anchorlocations.y, 'o', 'MarkerSize', 5);
+anchor1_indices = strcmp(raw_data.UWB_Anchor, '00280038:3136510B:34393732');
+anchor1_x = raw_data.UWB_x(anchor1_indices);
+anchor1_y = raw_data.UWB_y(anchor1_indices);
+plot(anchor1_x, anchor1_y, 'o', 'MarkerSize', 4);
+
+
+anchor2_indices = strcmp(raw_data.UWB_Anchor, '0047001D:3136510B:34393732');
+anchor2_x = raw_data.UWB_x(anchor2_indices);
+anchor2_y = raw_data.UWB_y(anchor2_indices);
+plot(anchor2_x, anchor2_y, 'o', 'MarkerSize', 4);
+
+
+anchor4_indices = strcmp(raw_data.UWB_Anchor, '002C0044:3136510B:34393732');
+anchor4_x = raw_data.UWB_x(anchor4_indices);
+anchor4_y = raw_data.UWB_y(anchor4_indices);
+plot(anchor4_x, anchor4_y, 'o', 'MarkerSize', 4);
+
+
+anchor5_indices = strcmp(raw_data.UWB_Anchor, '00440037:3136510B:34393732');
+anchor5_x = raw_data.UWB_x(anchor5_indices);
+anchor5_y = raw_data.UWB_y(anchor5_indices);
+plot(anchor5_x, anchor5_y, 'o', 'MarkerSize', 4);
+
+
+anchor7_indices = strcmp(raw_data.UWB_Anchor, '0040002D:3136510B:34393732');
+anchor7_x = raw_data.UWB_x(anchor7_indices);
+anchor7_y = raw_data.UWB_y(anchor7_indices);
+plot(anchor7_x, anchor7_y, 'o', 'MarkerSize', 4);
+
+
+plot(anchorlocations.x, anchorlocations.y, '>', 'MarkerSize', 10); % Plot with large triangles
+
+plot(raw_data.LiDAR_x, raw_data.LiDAR_y, 'k-', 'LineWidth', 2.5); 
+
 
 R = eul2rotm([anchorlocations.psi, anchorlocations.theta, anchorlocations.phi], 'ZYX');
 
@@ -34,10 +63,10 @@ end
 
 xlabel('X');
 ylabel('Y');
-title('Align PF UWB and LiDAR Trajectories');
+title('Raw UWB and LiDAR Trajectories');
 grid on;
 
-legend('UWB Trajectory (PF Results)', 'LiDar Trajectory (Ground Truth)');
+legend('Anchor 1', 'Anchor 2', 'Anchor 4', 'Anchor 5', 'Anchor 7', 'Anchor Locations', 'LiDar Trajectory (Ground Truth)');
 
 
 
