d3c7f4da0c223a7d02e393174a658281cadf8f87
Chadd Gaunt <Chadd.Gaunt@student.uts.edu.au>
2024-04-12 17:14:19 +1000

    Changed the input of process_data to contain the ID antenna that generates the observation, and the location of the base stations.

--- PF/load_and_discard_bad_data.m
+++ PF/load_and_discard_bad_data.m
@@ -1,9 +1,11 @@
 close all;
 
 data_table = readtable('Interpolated_data.xlsx');
+anchorlocations = readtable('UWB Device Locations AdNav.csv');
 
 T = data_table.timestamp';
 X = data_table.UWB_x';
 Y = data_table.UWB_y';
+anchor_ID = data_table.UWB_Anchor;
 
-process_data(X,Y,T)
+process_data(X,Y,T, anchor_ID, anchorlocations)
--- PF/process_data.m
+++ PF/process_data.m
@@ -1,4 +1,4 @@
-function process_data(X,Y,T)
+function process_data(X,Y,T, anchor_ID, anchorlocations)
     data = zeros(1081,2);
     M = zeros(50,2);
     particles = zeros(10*500,2);
@@ -11,6 +11,42 @@ function process_data(X,Y,T)
     hdl.PlotParticles = plot(particles(:,1),particles(:,2),'color','m','marker','.','lineStyle','none','MarkerSize',2);%,'MarkerEdgeColor','k','MarkerFaceColor','b');
     % axis equal;zoom on, grid on;hold on;
     axis ([-5 5 -5 5]); grid on;
+
+    R = eul2rotm([anchorlocations.psi, anchorlocations.theta, anchorlocations.phi], 'ZYX');
+
+
+    anchor_order = [1,5,2,7,4];
+    % color_order = ["#EDB120", "#0072BD", "#7E2F8E", "#A2142F", "#77AC30"];
+    color_order = ['r', 'g', 'b', 'm', 'y'];
+
+    for i = 1:length(anchorlocations.x)
+    % Extract the rotation matrix for the current point
+    R_current = R(:,:,i);
+    
+    % Define a fixed vector representing the initial direction (e.g., facing east)
+    v = [1; 0; 0];
+    
+    % Transform the fixed vector to the local coordinate system
+    local_v = R_current * v;
+    
+    % Extract the local x and y components of the transformed vector
+    local_x = local_v(1);
+    local_y = local_v(2);
+    
+    % Choose the color for the current triangle
+    color_index = mod(i, length(color_order)) + 1; % Cycling through the color order
+    color = color_order(color_index);
+    
+    ang = atan2(local_y,local_x);
+    ang = ang+pi;
+
+    fprintf(1,'%d: Angle %4.2f\n',i,ang+pi)
+    txt = sprintf("%d",anchor_order(i));
+    Veh = vehicle([anchorlocations.x(i), anchorlocations.y(i), ang]);
+    patch(Veh(1:3), Veh(4:6),  color,'FaceAlpha', 0.5);
+    text(anchorlocations.x(i)+0.4, anchorlocations.y(i),txt);
+
+end
 
     init=0;
 
